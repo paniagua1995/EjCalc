8c6dcca70f7de48e2329ba4f0e47be91bfab6d1e
IceMerman <jsjuanes51@gmail.com>
2017-12-13 22:53:36 -0500

    Doc

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import operaciones as op
 
 def main():
+    """
+    Metodo principal, lleve un ciclo hasta que se decida salir
+    pregunta por la operacion a realizar y los argumentos a operar
+    """
 
     return
 
