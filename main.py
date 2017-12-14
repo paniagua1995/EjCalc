97ce9bcd7dedd1fee632f274eb8416dfe68a1b8f
IceMerman <jsjuanes51@gmail.com>
2017-12-13 22:50:14 -0500

    Archivos iniciales

---
+++ main.py
@@ -0,0 +1,8 @@
+import operaciones as op
+
+def main():
+
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ operaciones.py
@@ -0,0 +1,41 @@
+def operacion():
+    """Esta función hace las veces de menú, aca se pregunta al
+    usuario que operacion se va a realizar.
+    Opciones:
+        Sumar       -> 1
+        Restar      -> 2
+        Multiplicar -> 3
+        Dividir     -> 4
+
+    retorna opt con la opcion, sino es valida se regresa 0
+    """
+    return opt
+
+def Sumar():
+    """Esta funcion suma los argumentos de entrada.
+    Quien la programe describe como son los argumentos de entrada
+    """
+
+    return
+
+def Restar():
+    """Esta funcion resta los argumentos de entrada.
+    Quien la programe describe como son los argumentos de entrada
+    """
+
+    return
+
+def Multiplicar():
+    """Esta funcion multiplica los argumentos de entrada.
+    Quien la programe describe como son los argumentos de entrada
+    """
+
+    return
+
+def Dividir():
+    """Esta funcion divide los argumentos de entrada.
+    Quien la programe describe como son los argumentos de entrada
+    """
+
+    return
+
